5af0be70d63b10321b1bac7a630d43fb941bd666
Dmytro-Kaflyk <d.o.kaflyk@student.khai.edu>
2025-05-16 21:38:48 +0300

    Signal processing, 3

--- SignalProcessing/SignalProcessing.py
+++ SignalProcessing/SignalProcessing.py
@@ -11,13 +11,11 @@ F_filter = 24
 a = 0
 b = 10
 signal_raw = np.random.normal(a, b, n)
-
 time = np.arange(n) / Fs
 
 w = F_max / (Fs / 2)
 sos = signal.butter(3, w, 'low', output='sos')
 filtered_signal = signal.sosfiltfilt(sos, signal_raw)
-
 
 def plot_signal(x, y, title, xlabel, ylabel):
     if not os.path.exists('./figures'):
@@ -31,7 +29,6 @@ def plot_signal(x, y, title, xlabel, ylabel):
     filepath = f'./figures/{title}.png'
     fig.savefig(filepath, dpi=600)
     plt.show()
-
 
 plot_signal(time, filtered_signal, "Відфільтрований_сигнал", "Час (с)", "Амплітуда")
 
@@ -128,4 +125,63 @@ plt.title("Залежність співвідношення сигнал-шум
 plt.grid(True)
 plt.tight_layout()
 plt.savefig('./figures/Співвідношення_сигнал_шум.png', dpi=600)
+plt.show()
+
+quantized_signals = []
+variance_quant = []
+snr_quant = []
+
+for M in [4, 16, 64, 256]:
+    delta = (np.max(filtered_signal) - np.min(filtered_signal)) / (M - 1)
+    quantize_signal = delta * np.round(filtered_signal / delta)
+    quantized_signals.append(list(quantize_signal))
+    quantize_levels = np.arange(np.min(quantize_signal), np.max(quantize_signal) + delta, delta)
+    quantize_bit = np.arange(0, M)
+    quantize_bit = [format(bits, '0' + str(int(np.log2(M))) + 'b') for bits in quantize_bit]
+    quantize_table = np.c_[quantize_levels[:M], quantize_bit[:M]]
+    fig, ax = plt.subplots(figsize=(14 / 2.54, M / 2.54))
+    table = ax.table(cellText=quantize_table, colLabels=['Значення сигналу', 'Кодова послідовність'], loc='center')
+    table.set_fontsize(14)
+    table.scale(1, 2)
+    ax.axis('off')
+    fig.savefig(f'./figures/Таблиця_квантування_для_{M}_рівнів.png', dpi=600)
+    bits = []
+    for signal_value in quantize_signal:
+        for index, value in enumerate(quantize_levels[:M]):
+            if np.round(np.abs(signal_value - value), 0) == 0:
+                bits.append(quantize_bit[index])
+                break
+    bits = [int(item) for item in list(''.join(bits))]
+    x_bits = np.arange(0, len(bits))
+    y_bits = bits
+    fig, ax = plt.subplots(figsize=(21 / 2.54, 14 / 2.54))
+    ax.step(x_bits, y_bits, linewidth=0.1)
+    ax.set_xlabel("Відлік", fontsize=14)
+    ax.set_ylabel("Біт", fontsize=14)
+    ax.set_title(f"Графік бітової послідовності для {M} рівнів", fontsize=14)
+    plt.tight_layout()
+    fig.savefig(f'./figures/Бітова_послідовність_{M}_рівнів.png', dpi=600)
+    plt.show()
+    var_diff_q = np.var(quantize_signal - filtered_signal)
+    variance_quant.append(var_diff_q)
+    snr_quant.append(np.var(filtered_signal) / var_diff_q)
+
+plt.figure(figsize=(8, 6))
+plt.plot([4, 16, 64, 256], variance_quant, marker='o')
+plt.xlabel("Кількість рівнів квантування M", fontsize=14)
+plt.ylabel("Дисперсія різниці", fontsize=14)
+plt.title("Залежність дисперсії різниці від M", fontsize=14)
+plt.grid(True)
+plt.tight_layout()
+plt.savefig('./figures/Дисперсія_різниці_квантування.png', dpi=600)
+plt.show()
+
+plt.figure(figsize=(8, 6))
+plt.plot([4, 16, 64, 256], snr_quant, marker='o')
+plt.xlabel("Кількість рівнів квантування M", fontsize=14)
+plt.ylabel("Співвідношення сигнал-шум", fontsize=14)
+plt.title("Залежність SNR від M", fontsize=14)
+plt.grid(True)
+plt.tight_layout()
+plt.savefig('./figures/Співвідношення_сигнал_шум_квантування.png', dpi=600)
 plt.show()
